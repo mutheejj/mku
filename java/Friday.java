37b8ed726fab6c9483eb6d6a74b75ca149a32b12
muthee <muthee@localhost.localdomain>
2024-06-28 13:02:44 +0300

    mku

--- java/Friday.java
+++ java/Friday.java
@@ -2,6 +2,30 @@
 
 public class Friday
 {
+    //class attribute
+    int coin = 5;
+    
+
+    static void sum(int p)
+    {
+        System.out.println("function/ method works" + " " + p);
+        System.out.println("use of recurion");
+        int sum = 10;
+        System.out.println("initail sum");
+        int k;
+    }
+
+    static int fact(int k)
+    {
+        if (k > 0)
+        {
+            return k + fact(k - 1);
+        }
+        else
+        {
+            return 0;
+        }
+    }
     public static void main(String[] args)
     {
         //how to print output
@@ -27,5 +51,21 @@ public class Friday
         //how to use short hand if
         String y = (z < 50) ? ("less") : ("greater");
         System.out.println(y);
+
+
+       //call a function
+       sum(12);
+
+
+       //recursion
+
+       int result = fact(10);
+       System.out.println(result);
+
+       //decrare an object
+       Friday myobject = new Friday();
+       System.out.println(myobject.coin);
+
+
     }
 }
