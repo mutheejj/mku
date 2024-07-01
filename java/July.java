f248ab27f57bb0c7b2987a9eb4aed3eef8c2d6b1
muthee <muthee@localhost.localdomain>
2024-07-01 16:25:22 +0300

    happy new month

---
+++ java/July.java
@@ -0,0 +1,24 @@
+public class July
+{
+    int y = 12;
+
+    public int fano(int x)
+    {
+        x = 12;
+        int sum = 0;
+        if (x >= 0)
+        {
+            return (sum + fano(x - 1));
+        }
+        return -1;
+    }
+
+    public static void main(String[] args)
+    {
+        July myobject = new July();
+        System.out.println("It is a new month");
+        System.out.println(myobject.y);
+
+        //how to call the object??
+    }
+}
