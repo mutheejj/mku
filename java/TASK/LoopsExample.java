a90dfc70aa1f62f27126efa0283b60911d332b53
muthee <muthee@localhost.localdomain>
2024-07-13 08:10:52 +0300

    added comments to the coode

--- java/TASK/LoopsExample.java
+++ java/TASK/LoopsExample.java
@@ -1,12 +1,18 @@
+
+//initialize the main class same to the name of the file LoopsExamle.java
 public class LoopsExample
 {
+    //This is where the program starts 
     public static void main(String[] args)
     {
+        //initialized a variable with varible name x data type is int and the value is o
         int x = 0;
-
-        while (x <= 10)
+        //the while loop
+        while (x <= 10)//the condition
+        //the block of  code to be excetuded if  the conditiion is false
         {
             System.out.println(x);
+            //incliment
             x++;
         }
     }
