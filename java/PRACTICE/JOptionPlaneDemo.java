d01d3b27140f27414102e548fc4ae1243cb61e24
muthee <muthee@localhost.localdomain>
2024-07-13 09:05:09 +0300

    added a readme file

---
+++ java/PRACTICE/JOptionPlaneDemo.java
@@ -0,0 +1,21 @@
+import java.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class JOptionPlaneDemo
+{
+    static JTextField uname;
+    public static void main(String[] args)
+    {
+        JFrame jf = new JFrame();
+        jf.setsize(250, 250);
+        Flowlayout = obj = new Flowlayout();
+        jf.setlayyout(ob);
+        Jlable lb.name = new JButton("submit");
+        myListerner obj = new myListerner();
+        submit.addActionLister(obj);
+        jf.add(lbname);
+        jf.add(submit);
+        jf.setvisible(true);
+    }
+}
