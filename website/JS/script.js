ada6f202e914db6dec0e145b1321710e9f04c4ed
muthee <muthee@localhost.localdomain>
2024-07-15 16:03:12 +0300

    added some js

---
+++ website/JS/script.js
@@ -0,0 +1,12 @@
+//this is how to replace an element or a section using Id
+document.getElementById("paragrap1").innerHTML= 5 + 5;
+
+//to write directly using javascript
+document.write(5 + 6);
+
+/* to alert
+in js*/
+
+window.alert("welcome");
+alert("thankyou");
+console.log(300);
