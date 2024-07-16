092e06e8e6d1834594cab4313255dfe076edcc5b
muthee <muthee@localhost.localdomain>
2024-07-16 20:25:40 +0300

    introduced python in my repo

---
+++ Python/adult.py
@@ -0,0 +1,11 @@
+name = input("Enter your name: ") # this is how to ask for user input
+
+print("Welcome " + name)  #print out
+
+age = int(input("Enter your age: "))
+
+if (age >= 18):  # use if statments
+    print("You are an adult")
+    print("You are Eligible to take an id")
+else:
+    print("you an a minor")
---
+++ Python/calc.py
@@ -0,0 +1,22 @@
+# This is how we add single line comments in python
+"""
+multtible line comments is intresting
+just use 
+three speech marks and close whe comments with another three
+"""
+
+# This is how to define a function
+# def fuctionname():
+#   indent the code
+
+def sum():
+    x = 20
+    #adding a variable
+    y = 30
+    #return value 
+    return x + y
+result = sum() # callinga a function
+print(result)
+
+name = "john" # a string
+print(name)
