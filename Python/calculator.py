ed1ae55444ebdd7aad75a24cb0486b28bd8feffe
muthee <muthee@localhost.localdomain>
2024-07-17 16:03:19 +0300

    i have added an incomplete progrm

---
+++ Python/calculator.py
@@ -0,0 +1,35 @@
+"""
+This calucator allows 
+    addation
+    subtruction
+    division
+    multiplication
+    square root
+    power
+
+"""
+
+import math
+
+def sum():
+    pass
+
+def sub():
+    pass
+
+def div():
+    pass
+
+def mult():
+    pass
+
+def sqr():
+    pass
+
+def pwr():
+    pass
+
+x = int( input("what type of operation do you wish to perform"))
+
+if (x == 1):
+    print("you selected 1")
